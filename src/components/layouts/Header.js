21930dee2fac432878c18aced5a55f3f6007b9f7
Aniket Sharma <aniket.sharma@moreideas.ae>
2024-10-31 15:13:20 +0400

    updated

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
@@ -6,17 +7,18 @@ export default function Header() {
     <header className="flex py-5 items-center justify-between">
       <nav className="flex text-white font-normal items-center">
         <Link className="text-primary font-normal text-2xl" href="/">
-          RENTAL
+          {/* <Image src="/images/logo.png" width={120} height={100} alt="Moreideas Logo" /> */}
+          OFFICE SPACE
         </Link>
       </nav>
       <nav className="flex gap-8 text-white font-normal items-center">
         <Link href={"/"}>Home</Link>
-        <Link href={""}>Offices</Link>
-        <Link href={""}>About</Link>
-        <Link href={""}>Contact</Link>
+        <Link href={"/"}>Offices</Link>
+        <Link href={"/"}>About</Link>
+        <Link href={"/"}>Contact</Link>
       </nav>
       <nav className="flex  text-white font-normal items-center">
-        <button className="black-btn">+971 54 998 2303</button>
+        <Link className="black-btn" href={"tel:+971549982303"}>+971 54 998 2303</Link>
       </nav>
     </header>
   );
